37dd4656fe07df5dd95ee38ff34a82d88faa0744
diegostaPy <diegostaPy@users.noreply.github.com>
2023-10-26 14:05:55 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="diegostaPy"
+original_name="proyectofinaldiplomado"
+original_urlname="ProyectofinalDiplomado"
+original_description="Awesome proyectofinaldiplomado created by diegostaPy"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv proyectofinaldiplomado $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
